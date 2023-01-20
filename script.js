5afd0c3c9be0fad5343019f06c365b94c0adb1b1
mbatorek7 <maegan.batorek@valpo.edu>
2023-01-19 21:55:08 -0800

    fixed styling and added questions variable

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
+//questions for quiz
+var questions = [
+  {
+    question: "What does HTML stand for?",
+    answers: {
+      a: 'Hyper Text Markup Language',
+      b: 'Hyper Trainer Marking Language',
+      c: 'Hyper Text Marketing Language',
+      d: 'Hyper Text Markup Leveler'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "<h1>Text</h1> is the correct way of making a header in HTML.",
+    answers: {
+      a: 'True',
+      b: 'False'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "Which of the following is the correct way to use the standard namespace in C++?",
+    answers: {
+      a: 'Using namespace standard;',
+      b: 'Using standard namespace;',
+      c: 'Using namespace std;',
+      d: 'Standard namespace used;'
+    },
+    correctAnswer: 'c'
+  }, 
+  {
+    question: "Which of these is NOT a programming language",
+    answers: {
+      a: 'Rust',
+      b: 'Ruby',
+      c: 'MATLAB',
+      d: 'Pearl'
+    },
+    correctAnswer: 'd'
+  }
+];
+
+//variables for rest of page
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 
 var timer;
 var timerCount;
 var isWin = false;
+var correctAnswers = 0;
 
 // The startGame function is called when the start button is clicked
 function startGame() {
